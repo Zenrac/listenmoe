7b74f7c57ca2e2e0e5b130369b9c8cb7b42bf9ba
Zenrac <zenrac@outlook.fr>
2019-08-15 14:18:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='listenmoe',
     packages=['listenmoe'],
-    version='v1.0.0',
+    version='v1.0.1',
     description='Unofficial python3 API wrapper to get information about'
     'the listen.moe live stream using aiohttp',
     author='Zenrac',
     author_email='zenrac@outlook.fr',
     url='https://github.com/Zenrac/listenmoe',
-    download_url='https://github.com/Zenrac/listenmoe/archive/v1.0.0.tar.gz',
+    download_url='https://github.com/Zenrac/listenmoe/archive/v1.0.1.tar.gz',
     keywords=['listenmoe'],
     include_package_data=True,
     install_requires=['aiohttp', 'asyncio']
